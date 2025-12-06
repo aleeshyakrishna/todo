151f0fb77f617cbcdfe02324287f3a5f6ae75d41
“Aleeshya” <aleeshya.cybpress@gmail.com>
2025-12-06 23:07:51 +0530

    fix : add no task available messge

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,23 @@
 import React from "react";
-//importing components
-import Todo from './Todo'
+import Todo from "./Todo";
 
 const TodoList = ({ todos, setTodos, filteredTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {filteredTodos.map((todo) => (
-          <Todo
-            todo={todo}
-            setTodos={setTodos}
-            todos={todos}
-            key={todo.id}
-            text={todo.text}
-          />
-        ))}
-        {/* {filteredTodos.map((todo) => (
-            <Todo key={todo.id} todo={todo} />
-          ))} */}
+        {filteredTodos.length > 0 ? (
+          filteredTodos.map((todo) => (
+            <Todo
+              todo={todo}
+              setTodos={setTodos}
+              todos={todos}
+              key={todo.id}
+              text={todo.text}
+            />
+          ))
+        ) : (
+          <p>No task available</p>
+        )}
       </ul>
     </div>
   );
